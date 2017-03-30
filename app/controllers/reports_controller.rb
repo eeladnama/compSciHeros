dc964018491ebf50e6e5f7d260a256dfd9d5ffd1
Zachary Paden <Zpaden@uncc.edu>
2017-03-30 18:25:40 +0000

    Set up show and edit files

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,10 @@ class ReportsController < ApplicationController
         @report = Report.new
     end
     
+    def show
+        @report = Report.find(params[:id])
+    end
+    
     def create
         @report = Report.new(report_params)
         
@@ -13,12 +17,33 @@ class ReportsController < ApplicationController
         end
     end
     
+    def edit
+        @report = Report.find(params[:id])
+    end
+    
+    def update
+        @report = Report.find(params[:id])
+        if @report.update(report_params)
+            redirect_to @report
+        else
+            render 'edit'
+        end
+    end
+    
     def index
         @reports = Report.all
     end
     
+    def destroy
+        @report =Report.find(params[:id])
+        @report.destroy
+        
+        redirect_to reports_path
+    end
+    
     private
     def report_params
-        params.require(:report).permit(:title, :text)
+        params.require(:report).permit(:title, :perpatrator, :location, :desc,
+        :email)
     end
 end
