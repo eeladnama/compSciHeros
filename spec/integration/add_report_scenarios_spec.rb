082176ec66a5ebbde6ebbd7b9dccce1012649e80
Zachary Paden <Zpaden@uncc.edu>
2017-04-17 16:05:37 +0000

    User adds a report scenario 1

---
+++ spec/integration/add_report_scenarios_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper.rb'
+
+feature"User adds a report" do
+    scenario "User successfully adds a new report from the listing reports page" do
+        visit reports_path
+        expect(page).to have_content("Listing Reports")
+        click_link "New Report"
+        expect(page).to have_content("New Report")
+        expect(page).to have_field("Title")
+        expect(page).to have_field("Location")
+        expect(page).to have_field("Desc")
+        expect(page).to have_field("Email")
+    end
+end
